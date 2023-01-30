122d587e9844d8679d415af96b2299726eb23bf7
SHIVAprasad77 <shivaprasad22432243@gmail.com>
2023-01-30 07:53:31 +0530

    first

---
+++ cal.js
@@ -0,0 +1,12 @@
+function display(value){
+    document.getElementById("result").value+=value;
+    
+}
+function clr(){
+    document.getElementById("result").value=" "
+}
+function equal(value){
+    let x=document.getElementById("result").value;
+    let y=eval(x)
+    document.getElementById("result").value=y;
+}
